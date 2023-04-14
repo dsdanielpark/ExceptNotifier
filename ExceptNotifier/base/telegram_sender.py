b15fa6d08f6df6deca48e1d381560d085f8f5fb4
parkminwoo <parkminwoo1991@gmail.com>
2023-04-15 00:55:22 +0900

    feat: update telegram sender

---
+++ ExceptNotifier/base/telegram_sender.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def send_telegram_msg(TOKEN, msg):
+    url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
+    req_dict = requests.get(url).json()
+    bot_id = dict(dict(dict(list(dict(req_dict).values())[1][0])['message'])['from'])['id']
+    bot_url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={bot_id}&text={msg}"
+    resp = requests.get(bot_url).json()
+
+    return resp
+
+
+if __name__ == "__main__": #No-QA
+
+    TOKEN = "xxxxx"
+    msg = "Test Message"
+    send_telegram_msg(TOKEN, msg)
