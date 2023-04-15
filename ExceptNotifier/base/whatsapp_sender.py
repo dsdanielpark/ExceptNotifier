f29daae1ad1a22da3c9af44812d407873c83357b
parkminwoo <parkminwoo1991@gmail.com>
2023-04-16 00:52:59 +0900

    feat: update whatsapp sender

---
+++ ExceptNotifier/base/whatsapp_sender.py
@@ -0,0 +1,26 @@
+import logging
+import requests
+
+
+def send_message(
+    msg, sender_phone_number_id, TOKEN, receiver_number, recipient_type="individual"
+):
+    url =  f"https://graph.facebook.com/v16.0/{sender_phone_number_id}/messages"
+    
+
+    data = { "messaging_product": "whatsapp", 
+            "to": receiver_number, 
+            "type": "template", 
+            "template": 
+                        { "name": "hello_world", 
+                          "language": 
+                                    { "code": "en_US" }
+                        } 
+            }
+
+    headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer {}".format(TOKEN),
+        }
+    res = requests.post(f"{url}", headers=headers, json=data)
+    return res
