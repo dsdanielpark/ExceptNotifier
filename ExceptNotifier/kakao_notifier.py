c26c98af457af9ff5f24cd56a865489ae5fde707
parkminwoo <parkminwoo1991@gmail.com>
2023-04-17 18:02:11 +0900

    fix: static method of kakao

--- ExceptNotifier/kakao_notifier.py
+++ ExceptNotifier/kakao_notifier.py
@@ -65,9 +65,37 @@ class ExceptKakao(BaseException):
 
 
     @staticmethod
-    def send_slack_message(URL, msg):
-        data = {'text':msg}
-        resp = requests.post(url=URL, json=data)
+
+    def send_kakao_msg(token_path: str, msg: str) -> dict:
+        """Send message to chat room through kakaotalk app's REST API.
+
+        :param token_path: Kakaotalk token path
+        :type token_path: str
+        :param msg: Message text
+        :type msg: str
+        :return: Response according to REST API request
+        :rtype: dict
+        """
+
+        with open(token_path,"r") as kakao:
+            tokens = json.load(kakao)
+
+        url="https://kapi.kakao.com/v2/api/talk/memo/default/send"
+        headers={
+            "Authorization" : "Bearer " + tokens["access_token"]
+        }
+        data = {
+            'object_type': 'text',
+            'text': msg,
+            'link': {
+                'web_url': 'https://developers.kakao.com',
+                'mobile_web_url': 'https://developers.kakao.com'
+            }
+        }
+        
+        data = {'template_object': json.dumps(data)}
+        resp = requests.post(url, headers=headers, data=data)
+
         return resp
 
 
