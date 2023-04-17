eda099ab71ea4276591a26f3cc2d5dafe5e9871e
parkminwoo <parkminwoo1991@gmail.com>
2023-04-18 02:49:17 +0900

    feat: update beep

---
+++ ExceptNotifier/base/beep_sender.py
@@ -0,0 +1,19 @@
+import os
+import platform
+import winsound
+
+def beep(sec=1, freq=1000) -> None:
+    """Make beep sound
+
+    :param sec: beep duration, defaults to 1
+    :type sec: int, optional
+    :param freq: beep fequency, defaults to 1000
+    :type freq: int, optional
+    """
+
+    sys = platform.system()
+
+    if sys == "Windows":
+        winsound.Beep(int(1000*sec), freq)
+    else:
+        os.system('play -nq -t alsa synth {} sine {}'.format(sec, freq))
