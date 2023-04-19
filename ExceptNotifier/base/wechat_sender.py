33d46a64b360ed61269c7f1b911e77f35354f669
parkminwoo <parkminwoo1991@gmail.com>
2023-04-20 02:11:41 +0900

    feat: update docstring of wechat sender

--- ExceptNotifier/base/wechat_sender.py
+++ ExceptNotifier/base/wechat_sender.py
@@ -1,6 +1,13 @@
 import requests
 
-def send_wechat_msg(_WECHAT_WEBHOOK_URL, msg):
+def send_wechat_msg(_WECHAT_WEBHOOK_URL: str, msg: str) -> None:
+    """Send message to wechat.
+
+    :param _WECHAT_WEBHOOK_URL: Wechat Webhook URL https://work.weixin.qq.com/api/doc/90000/90136/91770
+    :type _WECHAT_WEBHOOK_URL: str
+    :param msg: Message to send
+    :type msg: str
+    """
     msg_template = {
         "msgtype": "text", 
         "text": {
