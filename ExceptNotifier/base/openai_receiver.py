5cb28cb018e35ce344e0ac476c3db8a3a1d220c9
parkminwoo <parkminwoo1991@gmail.com>
2023-04-19 18:23:39 +0900

    feat: update openai api receiver

---
+++ ExceptNotifier/base/openai_receiver.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+# Copyright 2023 parkminwoo Authors.
+
+import openai
+
+def receive_openai_advice(_OPEN_AI_MODEL, _OPEN_AI_API, error_message):
+    openai.api_key = _OPEN_AI_API
+    model_engine = _OPEN_AI_MODEL
+
+    input_text = f"How can I fix this error? Give me short infomation about next error. error== {error_message}"
+    response = openai.ChatCompletion.create(
+                                            model=model_engine,
+                                            messages=[{"role": "user", "content": input_text }]
+                                            )
+    advice_msg = response['choices'][0]['message']['content']
+    return advice_msg
+
+
+if __name__ == "__main__":
+    _OPEN_AI_API = "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"
+    _OPEN_AI_MODEL = "gpt-3.5-turbo"
+    error_message = ' Cell 3 in ()----> 1 1/0 ZeroDivisionError: division by zero'
+    advice_msg = receive_openai_advice(_OPEN_AI_MODEL, _OPEN_AI_API, error_message)
+    print(advice_msg)
