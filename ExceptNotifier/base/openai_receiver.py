601532ed5ff57988affaa2c83de39ee9729dd482
parkminwoo <parkminwoo1991@gmail.com>
2023-05-13 04:35:36 +0900

    feat: customize prompt in open ai chatgpt

--- ExceptNotifier/base/openai_receiver.py
+++ ExceptNotifier/base/openai_receiver.py
@@ -49,7 +49,33 @@ def get_resp_openai_advice(
     openai.api_key = _OPEN_AI_API
     model_engine = _OPEN_AI_MODEL
 
-    input_text = f"How can I fix this error? Give me short infomation about next error. Let me know which code line and which code is incorrect. and try to make it fix or fix exampel. error== {error_message}"
+    
+def receive_openai_advice(
+    _OPEN_AI_MODEL: str, _OPEN_AI_API: str, error_message: str
+) -> str:
+    """Receive debugging information about your code from models in open ai.
+
+    :param _OPEN_AI_MODEL: model name of open ai
+    :type _OPEN_AI_MODEL: str
+    :param _OPEN_AI_API: API KEY value of open ai
+    :type _OPEN_AI_API: str
+    :param error_message: Error message
+    :type error_message: str
+    :return: Returns a description and example of the code, the location of the error, and a debugging code example.
+    :rtype: str
+    """
+
+    openai.api_key = _OPEN_AI_API
+    model_engine = _OPEN_AI_MODEL
+    if environ.get("_PROMPT_COMMAND") is None:
+        input_text = f"How can I fix this error? Give me short infomation about next error. Let me know which code line and which code is incorrect. and try to make it fix or fix exampel. error== {error_message}"
+    else:
+        input_text = f"{environ['_PROMPT_COMMAND']} error== {error_message}"
+    resp = openai.ChatCompletion.create(
+        model=model_engine, messages=[{"role": "user", "content": input_text}]
+    )
+    advice_msg = resp["choices"][0]["message"]["content"]
+    return advice_msg
     resp = openai.ChatCompletion.create(
         model=model_engine, messages=[{"role": "user", "content": input_text}]
     )
