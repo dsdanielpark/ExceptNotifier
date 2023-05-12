408eca5106cce437f1cdd2ca58cb8c7ba0ee65a0
parkminwoo <parkminwoo1991@gmail.com>
2023-05-13 02:16:11 +0900

    fix: without bard lang sys variable

--- ExceptNotifier/base/bard_receiver.py
+++ ExceptNotifier/base/bard_receiver.py
@@ -16,12 +16,13 @@ def receive_bard_advice(_BARD_API_KEY: str, error_message: str) -> str:
     environ["_BARD_API_KEY"] = _BARD_API_KEY
 
     if environ.get("_PROMPT_COMMAND") is None:
-        if environ["_BARD_ADVICE_LANG"]=='ko':
-            input_text = f"다음 error를 어떻게 고칠 수 있을까? 다음 에러에 대해서 설명해주고, 어떤 위치에서 어떤 코드가 잘못 되었는지 말해줘. 그리고 이 코드를 고칠 수 있는 예시 코드를 보여줘 error=={error_message}"
-        elif environ["_BARD_ADVICE_LANG"]=='jp':
-            input_text = f"次のエラーをどのように修正できますか？次のエラーについて説明し、どの場所でどのコードが間違っているかを教えてください。そして、このコードを修正できるサンプルコードを見せてください。 error=={error_message}"
-        else:
-            input_text = f"How can I fix this error? Give me short infomation about next error. Let me know which code line and which code is incorrect. and try to make it fix or fix example. error=={error_message}"
+        if environ.get["_BARD_ADVICE_LANG"] is not None:
+            if environ["_BARD_ADVICE_LANG"]=='ko':
+                input_text = f"다음 error를 어떻게 고칠 수 있을까? 다음 에러에 대해서 설명해주고, 어떤 위치에서 어떤 코드가 잘못 되었는지 말해줘. 그리고 이 코드를 고칠 수 있는 예시 코드를 보여줘 error=={error_message}"
+            elif environ["_BARD_ADVICE_LANG"]=='jp':
+                input_text = f"次のエラーをどのように修正できますか？次のエラーについて説明し、どの場所でどのコードが間違っているかを教えてください。そして、このコードを修正できるサンプルコードを見せてください。 error=={error_message}"
+            else:
+                input_text = f"How can I fix this error? Give me short infomation about next error. Let me know which code line and which code is incorrect. and try to make it fix or fix example. error=={error_message}"
     else:
         input_text = f"{environ['_PROMPT_COMMAND']} error=={error_message}" 
 
