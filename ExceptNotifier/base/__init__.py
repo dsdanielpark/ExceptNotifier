170fc6dac04425099deb4bb59bbf8777e9bf7f50
parkminwoo <parkminwoo1991@gmail.com>
2023-05-16 01:15:06 +0900

    refactor: change package structure

--- ExceptNotifier/base/__init__.py
+++ ExceptNotifier/base/__init__.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 # Copyright 2023 parkminwoo
-from ExceptNotifier.sender.kakao_sender import send_kakao_msg
-from ExceptNotifier.sender.mail_sender import send_gmail_msg
-from ExceptNotifier.sender.slack_sender import send_slack_msg
-from ExceptNotifier.sender.telegram_sender import send_telegram_msg
-from ExceptNotifier.sender.chime_sender import send_chime_msg
-from ExceptNotifier.sender.discord_sender import send_discord_msg
-from ExceptNotifier.sender.line_sender import send_line_msg
-from ExceptNotifier.sender.teams_sender import send_teams_msg
-from ExceptNotifier.sender.whatsapp_sender import send_whatsapp_msg
-from ExceptNotifier.sender.sms_sender import send_sms_msg
-from ExceptNotifier.sender.beep_sender import beep
-from ExceptNotifier.sender.openai_receiver import receive_openai_advice
-from ExceptNotifier.sender.wechat_sender import send_wechat_msg
-from ExceptNotifier.sender.openai_receiver import get_resp_openai_advice
+from ExceptNotifier.base.kakao_sender import send_kakao_msg
+from ExceptNotifier.base.mail_sender import send_gmail_msg
+from ExceptNotifier.base.slack_sender import send_slack_msg
+from ExceptNotifier.base.telegram_sender import send_telegram_msg
+from ExceptNotifier.base.chime_sender import send_chime_msg
+from ExceptNotifier.base.discord_sender import send_discord_msg
+from ExceptNotifier.base.line_sender import send_line_msg
+from ExceptNotifier.base.teams_sender import send_teams_msg
+from ExceptNotifier.base.whatsapp_sender import send_whatsapp_msg
+from ExceptNotifier.base.sms_sender import send_sms_msg
+from ExceptNotifier.base.beep_sender import beep
+from ExceptNotifier.base.openai_receiver import receive_openai_advice
+from ExceptNotifier.base.wechat_sender import send_wechat_msg
+from ExceptNotifier.base.openai_receiver import get_resp_openai_advice
 
 __all__ = [
     "send_kakao_msg",
